374304b515c2808f8c0aec6603f4ed433ee46074
LUCAS <chulyfra40@gmail.com>
2022-06-06 18:24:01 -0300

    Nuevas clases: Casillero, mapa y vehiculo

---
+++ src/main/java/casillero/Pozo.java
@@ -0,0 +1,18 @@
+package casillero;
+public class Pozo implements Casillero {
+    
+    @Override
+    public int calcularCostoDeMovimientos(Moto moto){
+        return 3;
+    }
+
+    /*@Override
+    public int calcularCostoDeMovimientos(Auto auto){
+        
+    }
+
+    @Override
+    public int calcularCostoDeMovimientos(CuatroPorCuatro cuatroPorCuatro){
+        
+    }*/
+}
