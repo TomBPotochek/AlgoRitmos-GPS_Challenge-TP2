ed582cb5fb00ff4a29e92fcfca9c4441cfc81d91
LUCAS <chulyfra40@gmail.com>
2022-06-07 12:33:43 -0300

    Pruebas del caso de uso Movimiento Invalido

---
+++ src/main/test/edu/fiuba/algo3/Movimientos/MovimientosTest.java
@@ -0,0 +1,21 @@
+package edu.fiuba.algo3.Movimientos;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import vehiculos.*;
+import casillero.*;
+import movimientos.Posicion;
+
+public class MovimientosTest {
+    @Test
+    public void testMotoIntentaMoverseFueraDelMapaYVuelveALaMismaPosicion(){
+        Moto moto = new Moto(1, 1);
+        Mapa mapa = new Mapa(3);
+        
+        Posicion posicionFinal = new Posicion(1,1);
+        moto.mover("Arriba", mapa);
+
+        assertEquals(moto.getPosicion(), posicionFinal);
+    }
+}
