3053dc7cfc3415f6d3c21178d7414ecb1971a470
LUCAS <chulyfra40@gmail.com>
2022-06-14 01:54:37 -0300

    Corrijo Errores de control policial

--- src/main/java/edu/fiuba/algo3/modelo/casillero/ControlPolicial.java
+++ src/main/java/edu/fiuba/algo3/modelo/casillero/ControlPolicial.java
@@ -8,34 +8,38 @@ public class ControlPolicial implements ElementoTablero {
 
     @Override
     public Efecto interactuar(Moto moto) {
-        int multiplicador = 0;
         if((new Random().nextDouble()) <= 0.8){// devuelve un numero de 0.00 a 1.00
-            multiplicador = 1;
+            return new Efecto(
+            (movimientos) ->  movimientos + 3,
+            moto);
         }
         return new Efecto(
-            (movimientos) ->  movimientos + (3*multiplicador),
+            (movimientos) ->  movimientos + 0,
             moto);
+        
     }
 
     @Override
     public Efecto interactuar(Auto auto) {
-        int multiplicador = 0;
         if((new Random().nextDouble()) <= 0.5){// devuelve un numero de 0.00 a 1.00
-            multiplicador = 1;
+            return new Efecto(
+            (movimientos) ->  movimientos + 3,
+            auto);
         }
         return new Efecto(
-            (movimientos) -> movimientos + (3*multiplicador),
+            (movimientos) ->  movimientos + 0,
             auto);
     }
 
     @Override
     public Efecto interactuar(CuatroPorCuatro cuatroPorCuatro) {
-        int multiplicador = 0;
         if((new Random().nextDouble()) <= 0.3){// devuelve un numero de 0.00 a 1.00
-            multiplicador = 1;
+            return new Efecto(
+            (movimientos) -> movimientos + 3,
+            cuatroPorCuatro);
         }
         return new Efecto(
-            (movimientos) -> movimientos + (3*multiplicador),
+            (movimientos) -> movimientos + 0,
             cuatroPorCuatro);
     }
 }
