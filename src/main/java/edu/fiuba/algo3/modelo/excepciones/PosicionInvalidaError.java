4b816c548b6a7e6fcdabfc643afd5e47472ec02b
Mario Adris <madris@fi.uba.ar>
2022-06-14 01:59:58 -0300

    Agregado de Jugador

---
+++ src/main/java/edu/fiuba/algo3/modelo/excepciones/PosicionInvalidaError.java
@@ -0,0 +1,5 @@
+package edu.fiuba.algo3.modelo.excepciones;
+
+public class PosicionInvalidaError extends RuntimeException {
+	
+}
---
+++ src/main/java/edu/fiuba/algo3/modelo/juego/Jugador.java
@@ -0,0 +1,53 @@
+package edu.fiuba.algo3.modelo.juego;
+
+import edu.fiuba.algo3.modelo.vehiculos.Vehiculo;
+import edu.fiuba.algo3.modelo.movimientos.*;
+import edu.fiuba.algo3.modelo.movimientos.Posicion;
+import edu.fiuba.algo3.modelo.casillero.Efecto;
+
+public class Jugador {
+	Vehiculo vehiculo;
+	int cantidadDeMovimientos;
+
+	public Jugador(Vehiculo unVehiculo) {
+		cantidadDeMovimientos = 0;
+		vehiculo = unVehiculo;
+	}	
+
+	public boolean estaEnPosicion(Posicion unaPosicion) {
+		return this.vehiculo.estaEnPosicion(unaPosicion);
+	}
+
+	public boolean cantidadDeMovimientosEs(int unEntero) {
+		return this.cantidadDeMovimientos == unEntero;
+	}
+
+	public void moverArriba() {
+		Movimiento movimiento = new MovArriba();
+		this.vehiculo.mover(movimiento);
+	}
+	
+	public void moverAbajo() {
+		Movimiento movimiento = new MovAbajo();
+		this.vehiculo.mover(movimiento);
+	}
+
+	public void moverDerecha() {
+		Movimiento movimiento = new MovDerecha();
+		this.vehiculo.mover(movimiento);
+	}
+
+	public void moverIzquierda() {
+		Movimiento movimiento = new MovIzquierda();
+		this.mover(movimiento);
+	}
+
+	private void mover(Movimiento movimiento) {
+		this.cantidadDeMovimientos += 1;
+
+		for (Efecto efecto: this.vehiculo.mover(movimiento)){
+			this.cantidadDeMovimientos = efecto.actualizar(this.cantidadDeMovimientos);
+			this.vehiculo = efecto.cambiar();
+		}
+	}
+}
