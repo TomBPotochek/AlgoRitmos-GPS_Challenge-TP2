d75f35d51582c2c2eb4bf83bc694e48b01f52907
TomBPotochek <tombpotochek@gmail.com>
2022-07-05 20:20:29 -0300

    sorpresata 2

--- src/main/java/edu/fiuba/algo3/modelo/casillero/Efecto/EfectoSorpresata.java
+++ src/main/java/edu/fiuba/algo3/modelo/casillero/Efecto/EfectoSorpresata.java
@@ -2,21 +2,27 @@ package edu.fiuba.algo3.modelo.casillero.Efecto;
 
 import edu.fiuba.algo3.modelo.excepciones.NoPuedeAtravesarObstaculoError;
 import edu.fiuba.algo3.modelo.juego.Jugador;
+import edu.fiuba.algo3.modelo.vehiculos.Vehiculo;
 
 public class EfectoSorpresata extends BaseEfectoDecorador {
     
     private Boolean frenarVehiculo;
+    private Vehiculo vehiculo;
 
-    public EfectoSorpresata(Boolean frenar){
+    public EfectoSorpresata(Boolean frenar, Vehiculo vehiculo){
         this.frenarVehiculo = frenar;
+        this.vehiculo = vehiculo;
     }
 
     @Override
     public void aplicarseSobre(Jugador jugador){
         super.aplicarseSobre(jugador);
 
+
         if (jugador.movimientosEsPar()){
-            //cambio
+            EfectoCambioDeVehiculo efecto = new EfectoCambioDeVehiculo();
+            efecto.setVehiculo(vehiculo);
+            efecto.aplicarseSobre(jugador);
         } else if (frenarVehiculo){
             throw new NoPuedeAtravesarObstaculoError();
         } else {
